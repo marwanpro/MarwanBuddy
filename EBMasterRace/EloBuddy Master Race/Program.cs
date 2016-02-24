b3aefcf08e17b02c42d30aaddec74a15e3e0d532
Marwan <marwan69120@live.fr>
2016-02-24 01:10:22 +0100

    LeagueSharp Addon Kappa

---
+++ EBMasterRace/EloBuddy Master Race/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using LeagueSharp;
+
+namespace EloBuddy_Master_Race
+{
+    internal class Program
+    {
+        public static Obj_AI_Base Player = ObjectManager.Player;
+        public static DateTime StartTime = DateTime.UtcNow;
+        public static int LastCheckTime;
+        public static int SpamInterval = 35;
+
+        private static void Main(string[] args)
+        {
+            Game.OnUpdate += Game_OnGameUpdate;
+        }
+
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (LastCheckTime + SpamInterval < CurrentTime())
+            {
+                LastCheckTime = CurrentTime();
+                Game.Say("/all Download your best script at EloBuddy.NET");
+                Game.Say("/all Dodge every skillshot, carry your ranked !");
+                Game.Say("/all No virus, 100% Safe, Undetectable");
+                Game.Say("http://elobuddy.net");
+            }
+        }
+
+        public static int CurrentTime()
+        {
+            var timeSpan = (DateTime.UtcNow - StartTime);
+            return (int)timeSpan.TotalSeconds;
+        }
+    }
+}
